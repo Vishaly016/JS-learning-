1038bb05b3d2403fc73eb5be5ee70feb060b7a52
Vishaly016 <iam.vishalyadav016@gmail.com>
2026-07-07 11:33:21 +0530

    Initial JavaScript learning files

---
+++ 01_basics/01_variableBasic.js
@@ -0,0 +1,9 @@
+const accountId = 23009
+let accountName = "Vishal"
+var accountCity = "Noida"
+accountEmail = "vishal@gmail.com"
+let accountMoney;
+
+console.log(accountId);
+console.log(accountCity);
+console.table([accountId, accountName, accountCity, accountEmail, accountMoney])
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,7 @@
+let num = "33abc"
+console.log(typeof num)
+
+let stringNum = Number(num)
+console.log(typeof stringNum)
+console.log(num)
+console.log(stringNum)
